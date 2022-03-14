18e8f76d8123932a80396980a021ff1efc025ba8
patrick mugambi <patrick@kali.pm.com>
2022-03-15 00:25:29 +0300

    create components/navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar(){
+    return (
+        <>
+        <header className="navbar">
+        <h2 className="heading"> Notebook </h2>
+        </header>
+        </>
+    )
+}
